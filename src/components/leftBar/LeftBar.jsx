40bb5a1aefdd4b50ce5fcc1b00dc171da35cb64e
SATRIO WIBISONO <satrio_wibisono@students.amikom.ac.id>
2023-06-16 15:07:51 +0700

    update komen, like, post

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,9 +1,6 @@
 import "./leftBar.scss";
 import Friends from "../../assets/1.png";
 import Groups from "../../assets/2.png";
-import Market from "../../assets/3.png";
-import Watch from "../../assets/4.png";
-import Memories from "../../assets/5.png";
 import Events from "../../assets/6.png";
 import Gaming from "../../assets/7.png";
 import Gallery from "../../assets/8.png";
@@ -38,18 +35,6 @@ const LeftBar = () => {
             <img src={Groups} alt="" />
             <span>Groups</span>
           </div>
-          {/* <div className="item">
-            <img src={Market} alt="" />
-            <span>Marketplace</span>
-          </div> */}
-          {/* <div className="item">
-            <img src={Watch} alt="" />
-            <span>Watch</span>
-          </div>
-          <div className="item">
-            <img src={Memories} alt="" />
-            <span>Memories</span>
-          </div> */}
         </div>
         <hr />
         <div className="menu">
